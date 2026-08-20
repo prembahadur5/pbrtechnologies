7568a78af2e73e77a4d31d116ef9bae5ebc66aae
prembahadur5 <prembahadur5@gmail.com>
2026-08-20 17:51:02 +0530

    ADD Js File

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'pbr-site-v2';
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
